0ed36f84557c0c33c7ab4fb4fd91439b3a9a171a
cesar <mi_buena_vida@hotmail.com>
2021-03-31 04:46:13 -0400

    movimiento con flechas

--- scene/game.js
+++ scene/game.js
@@ -1,24 +1,15 @@
-let UP = false;
-let DOWN = false;
-let LEFT = false;
-let RIGHT = false;
-
 let velocidad = 12;
 let cursor;
 let boy;
 
-
 export class Game extends Phaser.Scene {
-
 
     constructor() {
         super({ key: 'game' })
     }
 
     preload() {
-        // this.game.load.spritesheet('uniqueKey', 'img/huesos_sprite576×256.png', 256, 576);
         this.load.spritesheet('boy', 'img/huesos_sprite576×256.png', { frameWidth: 64, frameHeight: 64 }, 8);
-        // cursors = this.input.keyboard.createCursorKeys();
     }
 
     create() {
@@ -32,7 +23,8 @@ export class Game extends Phaser.Scene {
             frames: this.anims.generateFrameNames('boy', { start: 1, end: 8, zeroPad: 0 })
 
         }
-        )
+        );
+
         this.anims.create({
             key: 'left',
             repeat: -1,
@@ -40,7 +32,7 @@ export class Game extends Phaser.Scene {
             frames: this.anims.generateFrameNames('boy', { start: 10, end: 17, zeroPad: 0 })
 
         }
-        )
+        );
 
         this.anims.create({
             key: 'down',
@@ -49,7 +41,7 @@ export class Game extends Phaser.Scene {
             frames: this.anims.generateFrameNames('boy', { start: 19, end: 26, zeroPad: 0 })
 
         }
-        )
+        );
 
         this.anims.create({
             key: 'right',
@@ -58,116 +50,34 @@ export class Game extends Phaser.Scene {
             frames: this.anims.generateFrameNames('boy', { start: 27, end: 35, zeroPad: 0 })
 
         }
-        )
-
-        // this.input.keyboard.on('keyup-D', function (event) {
-        //     boy.anims.stop('right', true);
-        // });
-        // this.input.keyboard.on('keyup-A', function (event) {
-        //     boy.anims.stop('left', true);
-
-        // });
-        // this.input.keyboard.on('keyup-S', function (event) {
-        //     boy.anims.stop('down', true);
-        // });
-        // this.input.keyboard.on('keyup-W', function (event) {
-        //     boy.anims.stop('up', true);
-
-        // });
-
-        // this.input.keyboard.on('keydown-A', function (event) {
-        //     boy.anims.play('left', true);
-
-        // });
-
-        // this.input.keyboard.on('keydown-W', function (event) {
-        //     boy.anims.play('up', true);
-
-        // });
-
-        // this.input.keyboard.on('keydown-S', function (event) {
-        //     boy.anims.play('down', true);
-        // });
-
-
-        // this.input.keyboard.on('keydown-D', function (event) {
-        //     boy.anims.play('right', true);
-        // });
-    
+        );
 
     }
 
     update() {
 
-        // if (cursor.right.isDown) {
-        //     boy.anims.play('right', true);
-        // }
-        // if (cursor.right.isUp) {
-        //     boy.anims.stop('right', true);
-        // }
-
-        if(cursor.left.isDown || cursor.right.isDown || cursor.down.isDown || cursor.up.isDown){
-
-        
-        if (cursor.left.isDown) {
-            boy.anims.play('left', true);
-
-        } else if (cursor.right.isDown) {
-            boy.anims.play('right', true);
-
-        }
+        if (cursor.left.isDown || cursor.right.isDown || cursor.down.isDown || cursor.up.isDown) {
 
 
-        if (cursor.up.isDown) {
-            boy.anims.play('up', true);
-            
-        } else if (cursor.down.isDown) {
-            boy.anims.play('down', true);
-            
-        }
+            if (cursor.left.isDown) {
+                boy.anims.play('left', true);
 
-        }else {
+            } else if (cursor.right.isDown) {
+                boy.anims.play('right', true);
+
+            }
+
+            if (cursor.up.isDown) {
+                boy.anims.play('up', true);
+
+            } else if (cursor.down.isDown) {
+                boy.anims.play('down', true);
+
+            }
+
+        } else {
             boy.anims.stop();
         }
-        
-        
-     
-
-        // if(cursor.right.isUp){
-            
-        //     boy.anims.stop('right', false);
-        // }
-
-        // if(cursor.up.isUp){
-            
-        //     boy.anims.stop('up', true);
-        // }
- 
-        // if (cursor.right.isDown) {
-        //     boy.anims.play('right', true);
-
-        // } else if (cursor.right.isUp) {
-        //     boy.anims.stop('right', true);
-
-        // }
- 
-        // if (cursor.left.isUp) {
-        //     boy.anims.stop('left', true);
-        // }
-
-        // if (cursor.right.isDown) {
-        //     boy.anims.play('right', true);
-        // }
-        // if (cursor.right.isUp) {
-        //     boy.anims.stop('right', true);
-        // }
-
-        // if (cursor.right.isDown) {
-        //     boy.anims.play('right', true);
-        // }
-        // if (cursor.right.isUp) {
-        //     boy.anims.stop('right', true);
-        // }
 
     }
 
